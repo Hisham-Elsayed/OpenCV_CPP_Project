60c0434b00df3164bc72a80d73161a984d01a963
= <=>
2025-05-30 19:37:45 +0300

    project before class implementation

---
+++ opencv_project.h
@@ -0,0 +1,17 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/dnn.hpp>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+class Type
+{
+    
+
+
+private:
+ string imagePath;
+ string videoPath;
+
+
+};
